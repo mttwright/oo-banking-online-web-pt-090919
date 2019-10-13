060ac1c257e44f308fb6747b3fbd452528ef0b33
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 18:04:36 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
+  
+  attr_reader :name, :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(money)
+   @balance = @balance + money
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def balance=(num)
+    @balance = num
+  end
+  
+  def status=(status)
+    @status = status
+  end
+  
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,62 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :status
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = "pending"
+  end
+  
+  def amount
+    @transfer_amount
+  end
+  
+  def status=(status)
+    @status = status
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    
+    if @status == "pending"
+    
+      if self.valid? && @sender.balance > @transfer_amount
+        @sender.balance=(@sender.balance - @transfer_amount)
+        @receiver.balance=(@receiver.balance + @transfer_amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+        
+      end
+      
+    else
+      nil
+    end
+    
+  end
+  
+  def reverse_transfer
+  
+    if @status == "complete"
+      
+      @sender.balance = @sender.balance + @transfer_amount
+      @receiver.balance = @receiver.balance - @transfer_amount
+      @status = "reversed"
+      
+    else
+      nil
+    end
+  end
+  
+  
 end
