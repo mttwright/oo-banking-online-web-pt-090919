a9bb6f04bdce6ff46ed49996ee0623cc87023b36
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 17:54:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :status
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = "pending"
+  end
+  
+  def amount
+    @transfer_amount
+  end
+  
+  def status=(status)
+    @status = status
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    
+    if @status == "pending"
+    
+      if self.valid?
+        @sender.balance=(@sender.balance - @transfer_amount)
+        @receiver.balance=(@receiver.balance + @transfer_amount)
+        @status = "complete"
+      elsif @sender.valid?
+        
+      end
+      
+    else
+      nil
+    end
+    
+  end
+  
+  
+  
 end
